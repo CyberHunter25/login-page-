57ac6c084e04d43c5b91d2b828b1f3cd8001e8f5
Rushikesh Thakur <93699961+CyberHunter25@users.noreply.github.com>
2022-05-03 14:57:41 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,11 @@
+u='rushikesh'
+p='rushikesh'
+username = input("Please enter your username: ")
+if (username == u):
+ password = input("Please enter your password: ")
+ if (password == p):
+  print("logged in successfully")
+ else:
+    print("password incorrect")
+else:
+   print("username incorrect") 
